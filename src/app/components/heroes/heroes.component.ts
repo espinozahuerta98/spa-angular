d27f66db9b5129256485dd1d48e28fec290e3683
Carlos Daniel Espinoza Huerta <espinozahuerta98@gmail.com>
2021-02-26 18:45:38 -0500

    spa

---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from 'src/app/servicios/heroes.service';
+import { Route, Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html'
+})
+export class HeroesComponent implements OnInit {
+
+  heroes: Heroe[] = [];
+
+  constructor(private _heroesService:HeroesService, private router:Router) { }
+
+  ngOnInit(): void {
+    this.heroes = this._heroesService.getHeroes();
+  }
+
+  verHeroe(idx:number){
+    this.router.navigate( ['/heroe',idx] );
+  }
+
+}
